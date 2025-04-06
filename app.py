ca8dd0e5229aa9bef8ca773b0b689a0a7340b331
unknown <dhymazaldhyz2@gmail.com>
2025-04-06 22:30:04 +0700

    add: save scaler y

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 import numpy as np
 import tensorflow as tf
+import joblib
 
 app = Flask(__name__)
 
 # Load model
 model = tf.keras.models.load_model('model_rnn_konsumsi.keras')
+scaler_y = joblib.load('scaler_y.save')
 
 @app.route('/')
 def index():
@@ -13,10 +15,20 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-    input_data = np.array(data['data']).reshape(1, len(data['data']), 1)
-    prediction = model.predict(input_data)
-    return jsonify({'prediksi': float(prediction[0][0])})
-
+    data = request.json['data']  # ex: [108, 40, 40]
+    
+    # Ambil nilai terakhir sebagai input prediksi
+    input_data = np.array(data[-1:]).reshape(1, 1, 1)
+    
+    # Lakukan scaling (optional, kalau kamu ingin juga simpan scaler_X)
+    # Tapi kalau input sudah distandardkan manual, bisa langsung saja:
+    
+    pred_scaled = model.predict(input_data)
+    
+    # Inverse scaling ke bentuk asli
+    pred_original = scaler_y.inverse_transform(pred_scaled)
+    
+    return jsonify({'prediksi': float(pred_original[0][0])})
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
